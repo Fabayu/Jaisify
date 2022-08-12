5c2d45d61d94789a7b422d742b968a75ad6fc76b
Ayush Jaiswal <ayush.2024cs1134@kiet.edu>
2022-08-12 10:43:15 +0530

    first commit

---
+++ js/music_list.js
@@ -0,0 +1,51 @@
+let musicList = [
+    {
+        // name: "Rise up",
+        name: "Kehndi hundi si",
+        artist: "Ap Dhilon",
+        img: "img/Rise_up.png",
+        src: "audio/Kehndi Hundi Si.mp3",
+        love: false
+    },
+    {
+        name: "Fairytale",
+        artist: "Alexander Rybak",
+        img: "img/fairytale.jpg",
+        src: "audio/Alexander_Rybak-Fairytale.mp3",
+        love: false
+    },
+    {
+        name: "Impossible",
+        artist: "James Arthur",
+        img: "img/impossible.jpg",
+        src: "audio/James_Arthur-Impossible.mp3",
+        love: false
+    },
+    {
+        name: "Landscape",
+        artist: "Jarico",
+        img: "img/landscape.jpg",
+        src: "audio/Jarico-Landscape.mp3",
+        love: false
+    },
+    {
+        name: "Diamonds",
+        artist: "Rihanna",
+        img: "img/diamonds.jpg",
+        src: "audio/Rihanna-diamonds.mp3",
+        love: false
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+]
